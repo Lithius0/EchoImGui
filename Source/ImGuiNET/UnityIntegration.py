0733abf276c890962e0df7b09d1445aaa0920959
Lithius0 <aerosiiap@gmail.com>
2025-05-06 16:29:56 -0400

    Comment

--- Source/ImGuiNET/UnityIntegration.py
+++ Source/ImGuiNET/UnityIntegration.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-# This script converts ImGui's vectors to Unity vectors. 
+# This script converts ImGui's vectors to Unity vectors.
+# It just replaces the references to System.Numerics with UnityEngine.
+# Probably not super robust but it works in this case. 
 
 root_dir = Path(__file__).resolve().parent
 for path in root_dir.rglob("*.cs"):
