7dc3512ed2c57dd2918a5bd5e99d477d2d73a66e
Lithius0 <aerosiiap@gmail.com>
2025-05-06 16:28:17 -0400

    Replaced Numerics with UnityEngine

--- Source/ImGuiNET/UnityIntegration.py
+++ Source/ImGuiNET/UnityIntegration.py
@@ -3,7 +3,10 @@ from pathlib import Path
 # This script converts ImGui's vectors to Unity vectors. 
 
 root_dir = Path(__file__).resolve().parent
-for path in root_dir.rglob("*.gen.cs"):
+for path in root_dir.rglob("*.cs"):
     # print(path)
     with open(path, "r+") as f:
-        f.write(f.read().replace("using System.Numerics", "using UnityEngine"))
+        text = f.read()
+        f.seek(0)
+        f.write(text.replace("using System.Numerics", "using UnityEngine"))
+        f.truncate()
