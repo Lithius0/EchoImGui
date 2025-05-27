c6841f4af19dd2beff6a9ede345f760934f77ef0
Lithius0 <aerosiiap@gmail.com>
2025-05-27 15:30:56 -0400

    Fixed inconsistent crash on second start

--- Source/ImGuiController.cs
+++ Source/ImGuiController.cs
@@ -120,26 +120,33 @@ namespace EchoImGui
             try
             {
                 OnLayout?.Invoke();
-            }
-            finally
-            {
+                Constants.LayoutMarker.End();
+
                 // C++ asserts can't be caught in C# and will crash the editor. 
                 io.ConfigErrorRecoveryEnableAssert = false;
+
+                // Only try rendering if no errors happen in OnLayout.
+                // If an exception is thrown there, it's very likely the program is in an invalid state.
                 ImGui.Render();
+
                 // Turn off asserts only for user code.
                 // For EchoImGui's code, fail fast so it can be fixed.
                 io.ConfigErrorRecoveryEnableAssert = true;
-                Constants.LayoutMarker.End();
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
             }
         }
 
         private void OnEnable()
         {
             imGuiContext = ImGui.CreateContext();
-            imPlotContext = ImPlot.CreateContext();
             // This is needed because Dear ImGui and ImPlot are in separate dlls and do not share globals.
             // Might be worth having a single dll at some point the dll boundary causes a lot of issues.
             ImPlot.SetImGuiContext(imGuiContext);
+
+            imPlotContext = ImPlot.CreateContext();
 
             ImGuiIOPtr io = ImGui.GetIO();
 
